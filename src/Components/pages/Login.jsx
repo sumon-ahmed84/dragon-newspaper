0ee27057549a3cb5ddc53cb05ac923fef7938575
sumon-ahmed84 <sumonahmed275060@gmail.com>
2025-10-21 12:31:58 +0600

    design login and register page

--- src/Components/pages/Login.jsx
+++ src/Components/pages/Login.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router";
 
 const Login = () => {
-    return (
-        <div>
-            login page
+  return (
+    <div className="flex justify-center items-center w-11/12">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <div className="card-body">
+          <fieldset className="fieldset">
+            <h1 className="font-bold text-2xl text-center py-5">Login your account</h1>
+            <label className="label">Email</label>
+            <input type="email" className="input" placeholder="Email" />
+            <label className="label">Password</label>
+            <input type="password" className="input" placeholder="Password" />
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+            <button className="btn btn-neutral mt-4">Login</button>
+            <p className="text-center font-semibold">Dont’t Have An Account ? <Link to="/auth/register" className="text-secondary py-5">Register</Link></p>
+          </fieldset>
+
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/Components/pages/Register.jsx
+++ src/Components/pages/Register.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Register = () => {
     return (
-        <div>
-            this is register
+        <div className="flex justify-center items-center w-11/12">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <div className="card-body">
+          <fieldset className="fieldset">
+            
+            <h1 className="font-bold text-2xl text-center py-5">Register your account</h1>
+            <label className="label">Your Name</label>
+            <input type="text" className="input" placeholder="Enter Your Name" />
+            <label className="label">Photo URL</label>
+            <input type="text" className="input" placeholder="Enter Your Photo URL" />
+            <label className="label">Email</label>
+            <input type="email" className="input" placeholder="Email" />
+            <label className="label">Password</label>
+            <input type="password" className="input" placeholder="Password" />
+            <div className='p-2'>
+                
+              <span className="font-semibold"><input type="checkbox" /> Accept Term & Conditions</span>
+            </div>
+            <button className="btn btn-neutral mt-4">Register</button>
+            <p className="text-center font-semibold py-5">Already Have An Account ? <Link to="/auth/login" className="text-secondary">Login</Link></p>
+          </fieldset>
+
         </div>
+      </div>
+    </div>
     );
 };
 
